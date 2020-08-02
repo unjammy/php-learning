de3ba79ca14872a8ac298a617f5fac0b45d9eace
unjammy <clark@denunz.io>
2020-08-02 13:02:17 -0400

    html

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
-<?php
-$name = 'Freeman Bernstein';
-$greeting = 'Hello';
-echo $greeting . ', ' . $name;
-echo ' ';
-echo "{$greeting}, {$name}";
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Document</title>
+  <style type="text/css">
+  		header {
+  			background: #e3e3e3;
+  			padding: 2em;
+  			text-align: center;
+  		}
+  </style>
+</head>
+<body>
+	<header>
+		<h1><?php
+		$name = 'Freeman Bernstein';
+		$greeting = 'Hello';
+		echo "{$greeting}, {$name}"; 
+		?></h1>
+		<h1>
+			<?= "What are we doing today? " . htmlspecialchars($_GET['value'] . '?');?>
+		</h1>
+	</header>
+	
+</body>
+</html>
 
