b1921c0d1ee89ebbcfbf58ff1fc3375b3623ae33
unjammy <clark@denunz.io>
2020-08-02 16:52:22 -0400

    php logic

--- index.php
+++ index.php
@@ -1,29 +1,5 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Document</title>
-  <style type="text/css">
-  		header {
-  			background: #e3e3e3;
-  			padding: 2em;
-  			text-align: center;
-  		}
-  </style>
-</head>
-<body>
-	<header>
-		<h1><?php
-		$name = 'Freeman Bernstein';
-		$greeting = 'Hello';
-		echo "{$greeting}, {$name}"; 
-		?></h1>
-		<h1>
-			<?= "What are we doing today? " . htmlspecialchars($_GET['value'] . '?');?>
-		</h1>
-	</header>
-	
-</body>
-</html>
-
+<?php
+$greeting = 'Hello';
+$name = 'Freeman Bernstein';
+$answer = htmlspecialchars($_GET['value']);
+require 'index.template.php';
---
+++ index.template.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Document</title>
+  <style type="text/css">
+  		header {
+  			background: #e3e3e3;
+  			padding: 2em;
+  			text-align: center;
+  		}
+  </style>
+</head>
+<body>
+	<header>
+		<h1><?="{$greeting}, {$name}."; ?></h1>
+		<h1><?= "What are we doing today? {$answer}?"; ?></h1>
+	</header>
+	
+</body>
+</html>
